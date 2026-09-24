60abd8bcfeff32e020b5f9f7a1a5022186c491c0
Tom <tom.aliaga@duocuc.cl>
2026-09-24 16:44:58 -0300

    Corrección class -> className

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,6 +1,6 @@
 function Formulario() {
     return (
-    <form id="formulario-contacto" class="w-full max-w-md space-y-4">
+    <form id="formulario-contacto" className="w-full max-w-md space-y-4">
 
         <label for="correo">Correo:</label>
         <div>
@@ -8,19 +8,19 @@ function Formulario() {
         type="email"
         id="correo"
         name="Correo"
-        class="w-full p-2 rounded border border-gray-600 bg-gray-800"
+        className="w-full p-2 rounded border border-gray-600 bg-gray-800"
         ></input>
         </div>
 
-        <label for="mensaje" class="block mb-2">Mensaje:</label>
+        <label for="mensaje" className="block mb-2">Mensaje:</label>
         <textarea
         id="mensaje"
         name="Mensaje"
         rows="5"
-        class="w-full p-2 rounded border border-gray-600 bg-gray-800"
+        className="w-full p-2 rounded border border-gray-600 bg-gray-800"
         ></textarea> 
 
-        <button class="w-full bg-blue-600 hover:bg-blue-700 text-white p-2 rounded" type="submit">Enviar</button>
+        <button className="w-full bg-blue-600 hover:bg-blue-700 text-white p-2 rounded" type="submit">Enviar</button>
     </form>
     )
 
